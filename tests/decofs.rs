6859bb2fc24358aa8af62fc0c385bb842c5bf861
Simon Martin <SMartin@scottlogic.com>
2019-08-27 11:51:31 +0100

    cleanup build warning in test

--- tests/decofs.rs
+++ tests/decofs.rs
@@ -1,19 +1,6 @@
 use std::{fs, thread, time};
 use std::process::Command;  // Run programs
 use assert_cmd::prelude::*; // Add methods on commands
-use predicates::prelude::*; // Used for writing assertions
-
-#[test]
-fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::main_binary()?;
-    cmd.arg("foobar")
-        .arg("test/file/doesnt/exist");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("No such file or directory"));
-
-    Ok(())
-}
 
 #[test]
 fn can_readthru() -> Result<(), Box<dyn std::error::Error>> {
@@ -23,7 +10,7 @@ fn can_readthru() -> Result<(), Box<dyn std::error::Error>> {
     let mut child = cmd.spawn()?;
     thread::sleep(time::Duration::from_millis(1000));
     let actual = fs::read_to_string("t/hello");
-    Command::new("fusermount").arg("-u").arg("t").spawn()?.wait();
+    Command::new("fusermount").arg("-u").arg("t").spawn()?.wait()?;
     child.wait()?;
     assert_eq!(actual.unwrap(), "world");
     Ok(())
