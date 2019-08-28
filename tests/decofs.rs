16a17c2ab0538e62c18c24ecc492da74f4e81dad
Simon Martin <SMartin@scottlogic.com>
2019-08-28 08:49:07 +0100

    cleaner encapsulation of mounter

--- tests/decofs.rs
+++ tests/decofs.rs
@@ -1,17 +1,42 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::{fs, thread, time};
-use std::process::Command;  // Run programs
+use std::process::{Command, Child};  // Run programs
 use assert_cmd::prelude::*; // Add methods on commands
+
+struct FuseMounter {
+    child: Option<Child>
+}
+
+impl FuseMounter {
+    fn new() -> FuseMounter {
+        FuseMounter { child: None }
+    }
+
+    fn mount(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::main_binary()?;
+        cmd.arg("t").arg("t2");
+        self.child = Some(cmd.spawn()?);
+        thread::sleep(time::Duration::from_millis(1000));
+        Ok(())
+    }
+
+    fn umount(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        Command::new("fusermount").arg("-u").arg("t").spawn()?.wait()?;
+        let child = self.child.take();
+        child.unwrap().wait()?;
+        Ok(())
+    }
+}
 
 #[test]
 fn can_readthru() -> Result<(), Box<dyn std::error::Error>> {
+    let mut mounter = FuseMounter::new();
+    mounter.mount();
     fs::write("t2/hello", "world")?;
-    let mut cmd = Command::main_binary()?;
-    cmd.arg("t").arg("t2");
-    let mut child = cmd.spawn()?;
-    thread::sleep(time::Duration::from_millis(1000));
-    let actual = fs::read_to_string("t/hello");
-    Command::new("fusermount").arg("-u").arg("t").spawn()?.wait()?;
-    child.wait()?;
-    assert_eq!(actual.unwrap(), "world");
+    let actual = fs::read_to_string("t/hello")?;
+    assert_eq!(actual, "world");
+    mounter.umount();
     Ok(())
 }
