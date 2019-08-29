f171ea9e8d3981463cfc91565d6f548da2bb4c8a
Simon Martin <smartin@scottlogic.com>
2019-08-29 07:11:31 +0000

    Attempt at auto mount/unmount on mutex

--- tests/decofs.rs
+++ tests/decofs.rs
@@ -4,8 +4,10 @@ extern crate lazy_static;
 use std::{fs, thread, time};
 use std::process::{Command, Child};  // Run programs
 use assert_cmd::prelude::*; // Add methods on commands
-use std::sync::Mutex;
 use std::path::PathBuf;
+
+use std::sync::{Mutex, MutexGuard, PoisonError};
+use std::ops::{Deref, DerefMut};
 
 struct FuseMounter {
     child: Option<Child>
@@ -41,16 +43,62 @@ impl FuseMounter {
 }
 
 lazy_static! {
-    static ref MOUNTER: Mutex<FuseMounter> = Mutex::new(FuseMounter::new());
+    static ref MOUNTER: MutexMounter = MutexMounter::new();
+}
+
+struct MutexMounter {
+    mounter: Mutex<FuseMounter>
+}
+
+struct MutexMounterGuard<'a> {
+    inner_guard: MutexGuard<'a, FuseMounter>
+}
+
+impl MutexMounter {
+    fn new() -> MutexMounter {
+        MutexMounter { mounter: Mutex::new(FuseMounter::new()) }
+    }
+
+    fn lock(&self) -> Result<MutexMounterGuard, PoisonError<MutexMounterGuard>> {
+        MutexMounterGuard::new(self.mounter.lock().unwrap())
+    }
+}
+
+impl MutexMounterGuard<'_> {
+    fn new(mut inner_guard: MutexGuard<FuseMounter>) -> Result<MutexMounterGuard, PoisonError<MutexMounterGuard>> {
+        inner_guard.mount().unwrap();
+        Ok(MutexMounterGuard { inner_guard })
+    }
+}
+
+impl Drop for MutexMounterGuard<'_> {
+    fn drop(&mut self) {
+        self.inner_guard.umount().unwrap();
+    }
+}
+
+impl Deref for MutexMounterGuard<'_> {
+    type Target = FuseMounter;
+
+    fn deref(&self) -> &FuseMounter {
+        self.inner_guard.deref()
+    }
+}
+
+impl DerefMut for MutexMounterGuard<'_> {
+    fn deref_mut(&mut self) -> &mut FuseMounter {
+        self.inner_guard.deref_mut()
+    }
 }
 
 #[test]
 fn can_readthru() -> Result<(), Box<dyn std::error::Error>> {
-    let mut mounter = MOUNTER.lock()?;
-    mounter.mount();
+    let actual = {
+    let mounter = MOUNTER.lock()?;
     fs::write(mounter.source().join("hello"), "world")?;
     let actual = fs::read_to_string(mounter.target().join("hello"))?;
+    actual
+    };
     assert_eq!(actual, "world");
-    mounter.umount();
     Ok(())
 }
